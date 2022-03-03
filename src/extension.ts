fab465ed877191bb46613b8d3ef823bf21bdfa63
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-03-03 23:10:29 +0200

    update command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,12 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+// import * as cp from "child_process";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "git-worktrees" is now active!');
+    let disposable = vscode.commands.registerCommand("git-worktrees.worktree.list", () => {
+        vscode.window.showInformationMessage("Hello World from Git Worktrees!");
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('git-worktrees.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Git Worktrees!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
