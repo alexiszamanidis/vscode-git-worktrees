55de8f08380e9552f3e89e8e306b63abb31dbab2
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-03-06 23:51:46 +0200

    add await into show messages

--- src/helpers/vsCodeHelpers.ts
+++ src/helpers/vsCodeHelpers.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 import { APP_NAME } from "../constants/constants";
 
 export const showErrorMessage = async (errorMessage: string) =>
-    vscode.window.showErrorMessage(`${APP_NAME}: ${errorMessage}`);
+    await vscode.window.showErrorMessage(`${APP_NAME}: ${errorMessage}`);
 
 export const showInformationMessage = async (informationMessage: string) =>
-    vscode.window.showInformationMessage(`${APP_NAME}: ${informationMessage}`);
+    await vscode.window.showInformationMessage(`${APP_NAME}: ${informationMessage}`);
