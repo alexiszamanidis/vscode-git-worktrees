5fe74064933728d8678e719fb8762e1961e6ed62
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-04-16 12:46:11 +0300

    add git worktree add operations

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import gitWorktreeList from "./git/operations/worktree/gitWorktreeList";
 import gitWorktreeRemove from "./git/operations/worktree/gitWorktreeRemove";
+import gitWorktreeAdd from "./git/operations/worktree/gitWorktreeAdd";
 
 export function activate(context: vscode.ExtensionContext) {
     let gitWtList = vscode.commands.registerCommand("git-worktrees.worktree.list", async () => {
@@ -11,8 +12,13 @@ export function activate(context: vscode.ExtensionContext) {
         await gitWorktreeRemove();
     });
 
+    let gitWtAdd = vscode.commands.registerCommand("git-worktrees.worktree.add", async () => {
+        await gitWorktreeAdd();
+    });
+
     context.subscriptions.push(gitWtList);
     context.subscriptions.push(gitWtRemove);
+    context.subscriptions.push(gitWtAdd);
 }
 
 export function deactivate() {}
