2f16111ca1461dd0bd65e22697ec607f5e61df19
jonz94 <jody16888@gmail.com>
2022-05-26 22:34:20 +0800

    add windows support

--- src/helpers/stringHelpers.ts
+++ src/helpers/stringHelpers.ts
@@ -1,9 +1,9 @@
+import { dirname } from "path";
+
 export const removeFirstAndLastCharacter = (str: string): string => str.slice(1, -1);
 
 export const removeLastDirectoryInURL = (path: string): string => {
-    const pathParts = path.split("/");
-    pathParts.pop();
-    return pathParts.join("/");
+    return dirname(path);
 };
 
 export const removeNewLine = (string: string): string => {
